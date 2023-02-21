76d8f09f5fc069b5663092a1e0cdfe353f289df8
chenkun <kun.chen@yintech.cn>
2023-02-21 20:16:26 +0800

    first commit

---
+++ primary-white-board/src/main.ts
@@ -0,0 +1,27 @@
+import "./style.css";
+// canvas 工具样式
+import "./container.css";
+import { setupCounter } from "./counter";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div class="container">
+  <canvas id="canvas"></canvas>
+  <div class="tools">
+    <button type="button" class="button">Undo</button>
+    <button type="button" class="button">Clear</button>
+
+    <div class="colors">
+      <div class="color-field" style="background-color: black"></div>
+      <div class="color-field" style="background-color: red"></div>
+      <div class="color-field" style="background-color: blue"></div>
+      <div class="color-field" style="background-color: green"></div>
+      <div class="color-field" style="background-color: yellow"></div>
+    </div>
+
+    <input type="color" class="color-picker">
+    <input type="range" min="0.5" max="20" class="pen-range">
+  </div>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
