52e05d9eab1e4efad4f68a5a8cf46309ee2e356a
chenuvi <chenuvi@outlook.com>
2023-02-25 23:01:43 +0800

    todo canvas draw

--- primary-white-board/src/canvas.ts
+++ primary-white-board/src/canvas.ts
@@ -1,6 +1,15 @@
 export function canvasEvent(canvas: HTMLCanvasElement) {
+  canvas.width = window.innerWidth - 60
+  canvas.height = 400
   const ctx = canvas.getContext('2d')!
+  ctx.fillStyle = 'white'
+  ctx.fillRect(0,0, canvas.width,canvas.height)
   console.log('ctx: ', ctx);
+
+  let drawColor = 'black'
+  let drawWidth = '2'
+  
+
 
   canvas.addEventListener('mousedown', () => {
 
