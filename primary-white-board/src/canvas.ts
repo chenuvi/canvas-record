6b54583198b8316578297e111133ab18fb53017a
chenuvi <chenuvi@outlook.com>
2023-02-28 19:56:42 +0800

    Feat: draw functions

--- primary-white-board/src/canvas.ts
+++ primary-white-board/src/canvas.ts
@@ -1,21 +1,55 @@
 // init Vars
 let initBgColor = "white";
 let drawWidth = 2;
-let drawColor = "red";
+let drawColor = "black";
 let isDrawing = false;
-export function canvasEvent(canvas: HTMLCanvasElement) {
-  // init Setting
-  canvas.width = window.innerWidth - 60;
-  canvas.height = 400;
-  const ctx = canvas.getContext("2d")!;
-  ctx.fillStyle = initBgColor;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+let restoreArray = [];
+let index = -1;
 
-  canvas.addEventListener("touchmove", draw, false);
-  canvas.addEventListener("touchend", stop, false);
+const $ = document.querySelector.bind(document);
 
+const canvas: HTMLCanvasElement = $("#canvas")!;
+canvas.width = document.body.clientWidth - 100;
+canvas.height = document.body.clientHeight - 200;
+// canvas.height = 400;
+const ctx = canvas.getContext("2d")!;
+ctx.fillStyle = initBgColor;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+canvasEvent(canvas);
+
+const btnClear = $("#btnClear")!;
+btnClear.addEventListener("click", clear);
+const btnUndo = $("#btnUndo")!;
+btnUndo.addEventListener("click", undo);
+
+const colors = $("#colors");
+colors?.addEventListener("click", (e) => {
+  const colorItem = e.target as HTMLElement;
+  const bgColor = window
+    .getComputedStyle(colorItem, null)
+    .getPropertyValue("background-color");
+  drawColor = bgColor;
+});
+
+const $colorPicker = $("#color-picker")!;
+$colorPicker.addEventListener("change", (e) => {
+  const val = (e.currentTarget as HTMLInputElement).value;
+  drawColor = val;
+});
+
+const $penRange = $("#pen-range")!;
+$penRange.addEventListener("change", (e) => {
+  drawWidth = Number((e.currentTarget as HTMLInputElement).value);
+});
+
+function canvasEvent(canvas: HTMLCanvasElement) {
   canvas.addEventListener("mousedown", start, false);
+
   canvas.addEventListener("mousemove", draw, false);
+  canvas.addEventListener("touchmove", draw, false);
+
+  canvas.addEventListener("touchend", stop, false);
   canvas.addEventListener("mouseup", stop, false);
 
   function start(event: MouseEvent | TouchEvent) {
@@ -49,11 +83,14 @@ export function canvasEvent(canvas: HTMLCanvasElement) {
       isDrawing = false;
     }
   }
-
-  function clear() {
-    console.log("clear");
-    ctx.fillStyle = initBgColor;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }
 }
+
+function clear() {
+  ctx.fillStyle = initBgColor;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function undo() {}
+
+export {};
--- primary-white-board/src/main.ts
+++ primary-white-board/src/main.ts
@@ -1,27 +1,5 @@
 import "./style.css";
 // canvas 工具样式
 import "./container.css";
-import { canvasEvent } from "./canvas";
+import './canvas.ts'
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div class="container">
-  <canvas id="canvas"></canvas>
-  <div class="tools">
-    <button type="button" class="button">Undo</button>
-    <button type="button" class="button" onClick="clear()">Clear</button>
-
-    <div class="colors">
-      <div class="color-field" style="background-color: black"></div>
-      <div class="color-field" style="background-color: red"></div>
-      <div class="color-field" style="background-color: blue"></div>
-      <div class="color-field" style="background-color: green"></div>
-      <div class="color-field" style="background-color: yellow"></div>
-    </div>
-
-    <input type="color" class="color-picker">
-    <input type="range" min="0.5" max="20" class="pen-range">
-  </div>
-  </div>
-`;
-
-canvasEvent(document.querySelector<HTMLCanvasElement>("#canvas")!);
