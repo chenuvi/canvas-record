4a45fffec8fca4eee7af9b9e1913955827c73d1b
chenuvi <chenuvi@outlook.com>
2023-03-03 23:29:01 +0800

    Feat: create circle

--- move-circles/src/main.ts
+++ move-circles/src/main.ts
@@ -8,17 +8,52 @@ const windowHeight = window.innerHeight;
 canvas.height = windowHeight;
 canvas.width = windowWidth;
 canvas.style.backgroundColor = "#ff8";
+class Circle {
+  xpos: number;
+  ypos: number;
+  radius: number;
+  color: string;
+  text: string;
+  constructor(
+    xpos: number,
+    ypos: number,
+    radius: number,
+    color: string,
+    text: string
+  ) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.radius = radius;
+    this.color = color;
+    this.text = text;
+  }
 
-context.fillRect(20, 20, 100, 200);
-context.fillStyle = "red";
-context.fillRect(40, 220, 100, 200);
+  draw(context: CanvasRenderingContext2D) {
+    context.beginPath();
+    context.strokeStyle = this.color;
+    context.textAlign = "center";
+    context.textBaseline = "middle";
 
-// context.beginPath();
-context.strokeStyle = "blue";
-context.lineWidth = 20;
-context.arc(300, 300, 80, 0, Math.PI * 2, false);
-context.stroke();
-// context.closePath()
+    context.font = "20px Arial";
+    context.fillText(this.text, this.xpos, this.ypos);
+    context.lineWidth = 5;
+    context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+    context.stroke();
+    context.closePath();
+  }
+}
 
+let circleList = Array(10)
+  .fill(0)
+  .map((_, index) => {
+    let randomX = Math.random() * windowWidth;
+    let randomY = Math.random() * windowHeight;
+    return createCircle(
+      new Circle(randomX, randomY, 50, "black", String(index + 1))
+    );
+  });
+function createCircle(circle: Circle) {
+  circle.draw(context);
+}
 
 export {};
