cdb4eee302161d005c40c771a0f30b7509e31800
chenuvi <chenuvi@outlook.com>
2024-03-31 11:11:11 +0800

    Feat: hitter counter

--- move-circles/src/graphical/circle.ts
+++ move-circles/src/graphical/circle.ts
@@ -1,37 +1,71 @@
+import { WINDOW_HEIGHT, WINDOW_WIDTH } from "../constant";
+let hit_counter = 0;
 class Circle {
   xPos: number;
   yPos: number;
   radius: number;
   color: string;
   text?: string;
+  speed?: number;
+  private dx?: number;
+  private dy?: number;
+  ctx?: CanvasRenderingContext2D;
   constructor(
     xPos: number,
     yPos: number,
     radius: number,
     color: string = "black",
-    text?: string
+    text?: string,
+    speed?: number
   ) {
+    // this.xPos = xPos;
     this.xPos = xPos;
     this.yPos = yPos;
     this.radius = radius;
     this.color = color;
     this.text = text;
+    this.speed = speed;
+
+    if (this.speed) {
+      this.dx = 1 * this.speed;
+      this.dy = 1 * this.speed;
+    }
   }
 
   drawStoke(ctx: CanvasRenderingContext2D, lineWidth = 5) {
+    this.ctx = ctx;
     ctx.beginPath();
     if (this.text) {
       ctx.strokeStyle = this.color;
       ctx.font = "15px Arial";
       ctx.textAlign = "center";
       ctx.textBaseline = "middle";
-      ctx.fillText(this.text, this.xPos, this.yPos);
+      ctx.fillText(String(hit_counter), this.xPos, this.yPos);
       // ctx.strokeText(this.text, this.xPos, this.yPos);
     }
     ctx.lineWidth = lineWidth;
     ctx.arc(this.xPos, this.yPos, this.radius, 0, Math.PI * 2, false);
     ctx.stroke();
     ctx.closePath();
+  }
+
+  update() {
+    if (!this.ctx) return;
+    this.ctx.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    this.drawStoke(this.ctx);
+    if (this.xPos + this.radius > WINDOW_WIDTH || this.xPos - this.radius < 0) {
+      this.dx = -this.dx!;
+      hit_counter++;
+    }
+    if (
+      this.yPos + this.radius > WINDOW_HEIGHT ||
+      this.yPos - this.radius < 0
+    ) {
+      this.dy = -this.dy!;
+      hit_counter++;
+    }
+    this.xPos += this.dx!;
+    this.yPos += this.dy!;
   }
 }
 
--- move-circles/src/views/2_draw_circle.ts
+++ move-circles/src/views/2_draw_circle.ts
@@ -2,19 +2,37 @@ import { context } from "../common";
 import { Circle } from "../graphical";
 import { WINDOW_WIDTH, WINDOW_HEIGHT } from "../constant";
 
-let circleList = [];
-let crateCircle = function (circle: Circle) {
-  circle.drawStoke(context, 3);
-};
+let circle = new Circle(
+  WINDOW_WIDTH * Math.random(),
+  WINDOW_HEIGHT * Math.random(),
+  50,
+  "#c00",
+  String(1),
+  1
+);
 
-for (let i = 0; i < 20; i++) {
-  let circle = new Circle(
-    WINDOW_WIDTH * Math.random(),
-    WINDOW_HEIGHT * Math.random(),
-    50,
-    "#c00",
-    String(i + 1)
-  );
-  circleList.push(circle);
-  crateCircle(circleList[i]);
+createCircle(circle);
+function circleRun() {
+  requestAnimationFrame(circleRun);
+  circle.update();
 }
+circleRun();
+
+function createCircle(circle: Circle) {
+  circle.drawStoke(context, 3);
+}
+// const circleList = [];
+
+// createCircle(circle);
+// for (let i = 0; i < 20; i++) {
+//   let circle = new Circle(
+//     WINDOW_WIDTH * Math.random(),
+//     WINDOW_HEIGHT * Math.random(),
+//     50,
+//     "#c00",
+//     String(i + 1),
+//     1
+//   );
+//   circleList.push(circle);
+//   crateCircle(circleList[i]);
+// }
